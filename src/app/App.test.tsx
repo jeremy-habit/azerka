e9f5a8f04d9610b13d932e311fe9cb894cf162e3
jeremy-habit <jeremy.habit@gmail.com>
2022-04-06 15:32:51 +0200

    test corrected

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from '../app/App';
+import App from './App';
+import { HashRouter } from 'react-router-dom';
 
  test('renders learn react link', () => {
-  render(<App />);
+  render(<HashRouter><App /></HashRouter>);
   /* const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument(); */
 });
