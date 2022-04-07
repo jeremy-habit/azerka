56614148e206debc1e409bd7716d220251555b81
jeremy-habit <jeremy.habit@gmail.com>
2022-04-07 12:15:10 +0200

    title removed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,8 @@
 import React, { ReactElement } from "react";
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
 
 export default function Home(): ReactElement {
   return (
     <div>
-      <Title>GROS TITLE</Title>
       <h2>Home</h2>
     </div>
   );
